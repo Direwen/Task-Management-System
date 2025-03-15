6759544429b496332aa85415a836690d3c28d5aa
Direwen <direwensik@gmail.com>
2025-03-15 23:59:24 +0630

    Implemented the bulk update

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.tasks_dashboard, name="task_list"),
-    path("update/<int:pk>", views.update_task, name="update_task")
+    path("update/<int:pk>", views.update_task, name="update_task"),
+    path("bulk-update/", views.bulk_update_tasks, name="bulk_update_tasks"),
 ]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from .forms import TaskForm
 from .models import Task
 
@@ -24,10 +25,11 @@ def update_task(request, pk):
         task.save()
         return render(request, "partials/row.html", {"task": task, "status": Task.STATUS_CHOICES})
     
-def bulk_update_task(request):
+def bulk_update_tasks(request):
     if request.method == "POST":
         task_ids = request.POST.getlist("task_ids")
         new_status = request.POST.get("bulk_status")
+        print(f">>>>>>>>>>>> {task_ids} {new_status}")
         if task_ids and new_status:
             Task.objects.filter(pk__in=task_ids).update(status=new_status)
             tasks = Task.objects.all()
@@ -35,7 +37,13 @@ def bulk_update_task(request):
             page_number = request.GET.get("page", 1)
             page = paginated.get_page(page_number)
             context = {
-                "tasks" : page,
-                "status" : Task.STATUS_CHOICES
+                "tasks": page,
+                "status": Task.STATUS_CHOICES
             }
             return render(request, "partials/table.html", context)
+        else:
+            print("Invalid request NO MORE IDS")
+            return HttpResponse("Invalid request: No task IDs or status provided", status=400)
+    else:
+        print("Invalid request")
+        return HttpResponse("Invalid request: Must be POST", status=400)
